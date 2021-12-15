be0dfd26400dac9d993f0aa8b6867092ff7f044d
jahongir95 <jakhongirsagdullaev95@gmail.com>
2021-12-15 12:23:42 +0500

    scroll reveal effect added

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,25 @@ languagePointer.onclick = () => {
     languageWrapper.classList.toggle('open')
     languagePointer.classList.toggle('rotate')
 }
+
+
+
+
+/* SCROLL REVEAL JS */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.hero__left, .hero__right, .app__responsive,
+            .about__info-block, .about__right,
+            .pills__right-one, .pills__left-one,
+            .pills__right-two, .pills__left-two,
+            .pills__left-three, .pills__right-three,
+            .download__inner, .help__inner,
+            .pair, .devider, .footer__bottom `
+            , {
+    interval: 200
+})

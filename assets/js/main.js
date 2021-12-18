ac61d5af5571de50f61ac16575aeb60602466092
jahongir95 <jakhongirsagdullaev95@gmail.com>
2021-12-18 15:44:15 +0500

    new change

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ allLangs.forEach(item => {
 /* SCROLL REVEAL JS */
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '50px',
+    distance: '30px',
     duration: 2000,
     reset: true
 });

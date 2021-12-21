33e59c9db9af6692d513ddd1a9679d5016c5d4a1
jahongir95 <jakhongirsagdullaev95@gmail.com>
2021-12-21 18:28:44 +0500

    next change

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,23 +75,23 @@ window.addEventListener("scroll", function () {
   lastScrollTop = scrollTop
 })
 
+
 /* SCROLL REVEAL JS */
 const sr = ScrollReveal({
-  origin: "top",
+  origin: 'bottom',
   distance: "30px",
-  duration: 2000,
+  duration: 1000,
   reset: true,
 })
 
 sr.reveal(
   ` .hero__right, .hero__text, .app__responsive,
             .about__left, .about__right,
-            .pills__right-one, .pills__left-one,
-            .pills__right-two, .pills__left-two,
-            .pills__left-three, .pills__right-three,
+            .pills__inner,
             .download__inner, .help__inner,
             .pair, .devider, .footer__bottom`,
   {
     interval: 200,
   }
 )
+
